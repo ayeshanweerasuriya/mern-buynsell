0592e4ee15135fae2e1a1bb5c11d4570a55a3afe
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-02-13 23:44:45 +0530

    implement signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,56 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", formData);
+  };
+
+  console.log(formData);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">SignUp</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className=" p-3 rounded-lg text-black"
+          id="username"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="email"
+          placeholder="email"
+          className=" p-3 rounded-lg text-black "
+          id="email"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          className=" p-3 rounded-lg  text-black"
+          id="password"
+          onChange={handleChange}
+        ></input>
+        <button className="bg-slate-600 text-white font-bold p-4 rounded-lg uppercase hover:bg-slate-700 disabled:opacity-70">
+          Sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 justify-center">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
