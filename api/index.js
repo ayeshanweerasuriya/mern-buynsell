c3ea9255e518a9dd02877df71d8f28de3ec5bab9
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-02-13 19:19:39 +0530

    create middleware to handle function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config(); // initialize dotenv
 
@@ -14,10 +16,22 @@ mongoose
   });
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running in port 3000");
 });
 
-app.get("/test", (req, res) => {
-  res.send("Hello World");
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+// create middileware to handle errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
